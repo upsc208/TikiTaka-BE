96ee865405df4ccedb07a6e6d704c2eec548ba66
Hojun Im <ghwnsdla8094@gmail.com>
2025-01-23 14:56:26 +0900

    [TKT-15] feat: 공통 엔티티 추가

--- src/main/java/com/trillion/tikitaka/TikitakaApplication.java
+++ src/main/java/com/trillion/tikitaka/TikitakaApplication.java
@@ -2,8 +2,10 @@ package com.trillion.tikitaka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TikitakaApplication {
 
 	public static void main(String[] args) {
