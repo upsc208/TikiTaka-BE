a383b904bde6d933cac3865c919763afb56ab5d3
Hojun Im <ghwnsdla8094@gmail.com>
2025-01-23 15:21:34 +0900

    [TKT-23] feat: 사용자 엔티티 추가

---
+++ src/main/java/com/trillion/tikitaka/user/domain/Role.java
@@ -0,0 +1,7 @@
+package com.trillion.tikitaka.user.domain;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    USER
+}
---
+++ src/main/java/com/trillion/tikitaka/user/domain/User.java
@@ -0,0 +1,76 @@
+package com.trillion.tikitaka.user.domain;
+
+import com.trillion.tikitaka.global.common.DeletedBaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.LockedException;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends DeletedBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime lastLoginAt = null;
+
+    private LocalDateTime lastPasswordChangedAt = null;
+
+    private int loginFailCount = 0;
+
+    private boolean locked = false;
+
+    private LocalDateTime lockExpireAt = null;
+
+    @Builder
+    public User(String username, String email, String password, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void handleLoginFailure() {
+        this.loginFailCount++;
+        if (this.loginFailCount >= 5) {
+            this.loginFailCount = 0;
+            this.locked = true;
+            this.lockExpireAt = LocalDateTime.now().plusMinutes(30);
+        }
+    }
+
+    public void resetLoginFailCount() {
+        this.loginFailCount = 0;
+        this.locked = false;
+        this.lockExpireAt = null;
+    }
+
+    public void updateLastLoginAt(LocalDateTime time) {
+        this.lastLoginAt = time;
+    }
+
+    public void updateLastPasswordChangedAt(LocalDateTime time) {
+        this.lastPasswordChangedAt = time;
+    }
+}
