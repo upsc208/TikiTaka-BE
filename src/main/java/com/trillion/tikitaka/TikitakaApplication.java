62c93f6531a7ba6445ea069f4c9a781f245aa326
Hojun Im <ghwnsdla8094@gmail.com>
2025-01-23 14:55:33 +0900

    [TKT-15] feat: 기본 프로젝트 추가

---
+++ src/main/java/com/trillion/tikitaka/TikitakaApplication.java
@@ -0,0 +1,13 @@
+package com.trillion.tikitaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikitakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikitakaApplication.class, args);
+	}
+
+}
