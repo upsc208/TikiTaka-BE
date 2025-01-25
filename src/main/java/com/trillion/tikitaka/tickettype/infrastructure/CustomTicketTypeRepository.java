b2c9a94668ef5a1a5b653c67af2a3ecf7901fbbb
Hojun Im <ghwnsdla8094@gmail.com>
2025-01-25 14:15:12 +0900

    [TKT-32] feat: 티켓 타입 수정 시 중복 조회 쿼리문 추가

--- src/main/java/com/trillion/tikitaka/tickettype/infrastructure/CustomTicketTypeRepository.java
+++ src/main/java/com/trillion/tikitaka/tickettype/infrastructure/CustomTicketTypeRepository.java
@@ -1,9 +1,12 @@
 package com.trillion.tikitaka.tickettype.infrastructure;
 
+import com.trillion.tikitaka.tickettype.domain.TicketType;
 import com.trillion.tikitaka.tickettype.dto.response.TicketTypeListResponse;
 
 import java.util.List;
 
 public interface CustomTicketTypeRepository {
     List<TicketTypeListResponse> getTicketTypes(Boolean active);
+
+    List<TicketType> findByIdAndNameCheck(Long id, String name);
 }
--- src/main/java/com/trillion/tikitaka/tickettype/infrastructure/CustomTicketTypeRepositoryImpl.java
+++ src/main/java/com/trillion/tikitaka/tickettype/infrastructure/CustomTicketTypeRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.trillion.tikitaka.tickettype.infrastructure;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.trillion.tikitaka.tickettype.domain.TicketType;
 import com.trillion.tikitaka.tickettype.dto.response.QTicketTypeListResponse;
 import com.trillion.tikitaka.tickettype.dto.response.TicketTypeListResponse;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +27,25 @@ public class CustomTicketTypeRepositoryImpl implements CustomTicketTypeRepositor
                 .from(ticketType)
                 .where(activeEq(active))
                 .fetch();
+    }
+
+    @Override
+    public List<TicketType> findByIdAndNameCheck(Long id, String name) {
+        return queryFactory
+                .selectFrom(ticketType)
+                .where(
+                        typeIdEq(id)
+                        .or(typeNameEq(name))
+                )
+                .fetch();
+    }
+
+    private static BooleanExpression typeIdEq(Long id) {
+        return ticketType.id.eq(id);
+    }
+
+    private static BooleanExpression typeNameEq(String name) {
+        return ticketType.name.eq(name);
     }
 
     private BooleanExpression activeEq(Boolean active) {
