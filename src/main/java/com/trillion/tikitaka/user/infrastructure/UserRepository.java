77b12a67e916850b21ec9258f834f4b92efb9158
kangai1 <kmj104506@gmail.com>
2025-02-03 07:04:09 +0900

    [TKT-52] feat: 삭제되지 않은 계정만 조회 추가

--- src/main/java/com/trillion/tikitaka/user/infrastructure/UserRepository.java
+++ src/main/java/com/trillion/tikitaka/user/infrastructure/UserRepository.java
@@ -2,9 +2,12 @@ package com.trillion.tikitaka.user.infrastructure;
 
 import com.trillion.tikitaka.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByIdAndDeletedAtIsNull(Long id);
 }
