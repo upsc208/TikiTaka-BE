a90edf20719bad62decc6ef39966209d05a45dcf
kangai1 <kmj104506@gmail.com>
2025-02-03 06:38:47 +0900

    [TKT-37] refactor: 중복 삭제

--- src/main/java/com/trillion/tikitaka/user/infrastructure/UserPasswordHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trillion.tikitaka.user.infrastructure;
-
-import com.trillion.tikitaka.user.domain.UserPasswordHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPasswordHistoryRepository extends JpaRepository<UserPasswordHistory, Long> {
-}
