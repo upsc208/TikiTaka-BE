bf9ad5e538fa14b07b4b0c0e71d6e5d5f4380ea3
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-06 00:15:08 +0900

    [TKT-98] feat: 사용자 프로필 이미지 설정 기능 추가

--- src/main/java/com/trillion/tikitaka/global/exception/ErrorCode.java
+++ src/main/java/com/trillion/tikitaka/global/exception/ErrorCode.java
@@ -90,7 +90,7 @@ public enum ErrorCode {
     INVALID_FILE_NAME(HttpStatus.BAD_REQUEST, "O004", "유효하지 않은 파일명입니다."),
     INVALID_FILE_EXTENSION(HttpStatus.BAD_REQUEST, "O005", "허용되지 않는 파일 확장자입니다."),
     FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "O006", "파일을 찾을 수 없습니다."),
-
+    FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "O007", "파일 삭제 중 오류가 발생했습니다."),
     ;
 
     private final HttpStatus httpStatus;
