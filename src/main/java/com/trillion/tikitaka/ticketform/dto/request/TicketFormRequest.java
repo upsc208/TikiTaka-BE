a8d944936674fa91ea0dd254d34c640303669ba6
김기훈 <upscman@gmail.com>
2025-02-05 09:49:33 +0900

    [TKT-86] feat: 티켓 폼 테이블에 mustDescription필드 추가 완료

--- src/main/java/com/trillion/tikitaka/ticketform/dto/request/TicketFormRequest.java
+++ src/main/java/com/trillion/tikitaka/ticketform/dto/request/TicketFormRequest.java
@@ -11,6 +11,10 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 public class TicketFormRequest {
 
+    @NotNull(message = "필수 내용을 입력해주세요.")
+    @Length(max = 1000, message = "필수 내용은 최대 1000자까지 입력 가능합니다.")
+    private String mustDescription;
+
     @NotNull(message = "티켓 폼을 입력해주세요.")
     @Length(max = 5000, message = "티켓 폼은 최대 5000자까지 입력 가능합니다.")
     private String description;
