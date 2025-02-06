b10f15471edb095d60361a57d4a78d223ccac77b
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-07 03:18:42 +0900

    [TKT-123] feat: 알림 로그 엔티티 생성

---
+++ src/main/java/com/trillion/tikitaka/notification/domain/Notification.java
@@ -0,0 +1,56 @@
+package com.trillion.tikitaka.notification.domain;
+
+import com.trillion.tikitaka.global.common.BaseEntity;
+import com.trillion.tikitaka.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String messageJson;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationStatus status;
+
+    @Column(nullable = false)
+    private String errorMessage;
+
+    @Builder
+    public Notification(User receiver, String messageJson, NotificationType type, NotificationStatus status) {
+        this.receiver = receiver;
+        this.messageJson = messageJson;
+        this.type = type;
+        this.status = status;
+        this.errorMessage = "";
+    }
+
+    public void updateStatus(NotificationStatus status) {
+        this.status = status;
+    }
+
+    public void updateMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
