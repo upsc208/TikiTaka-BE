745b187f0e1702a0a8e99b7eb6bdb80116d8f434
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-06 21:06:15 +0900

    [TKT-107] fix: 읽기 전용 오류 수정

--- src/main/java/com/trillion/tikitaka/global/config/SecurityConfig.java
+++ src/main/java/com/trillion/tikitaka/global/config/SecurityConfig.java
@@ -33,7 +33,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
