dc9f3ee06a0ce601d93ddb49a12c23bfe63d3709
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-06 15:21:01 +0900

    [TKT-107] fix: CORS 오리진 모두 허용

--- src/main/java/com/trillion/tikitaka/global/config/SecurityConfig.java
+++ src/main/java/com/trillion/tikitaka/global/config/SecurityConfig.java
@@ -128,9 +128,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "X-Requested-With"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
         corsConfiguration.setAllowCredentials(true);
 
