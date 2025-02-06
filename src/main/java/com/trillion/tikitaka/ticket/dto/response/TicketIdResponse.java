df24b133fd724dae60f3da868127e554bade9ec5
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-07 01:28:00 +0900

    [TKT-121] feat: 티켓 아이디 반환 시 DTO로 반환

---
+++ src/main/java/com/trillion/tikitaka/ticket/dto/response/TicketIdResponse.java
@@ -0,0 +1,11 @@
+package com.trillion.tikitaka.ticket.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketIdResponse {
+    private Long ticketId;
+}
