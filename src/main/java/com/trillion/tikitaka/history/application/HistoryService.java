94c78841f1b89bdb93b30ac809802333246cb2b3
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-07 18:16:33 +0900

    [TKT-135] refactor: 티켓 내역 관련 로직 로깅

--- src/main/java/com/trillion/tikitaka/history/application/HistoryService.java
+++ src/main/java/com/trillion/tikitaka/history/application/HistoryService.java
@@ -7,11 +7,13 @@ import com.trillion.tikitaka.history.infrastructure.HistoryRepository;
 import com.trillion.tikitaka.ticket.domain.Ticket;
 import com.trillion.tikitaka.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -19,11 +21,13 @@ public class HistoryService {
     private final HistoryRepository historyRepository;
 
     public Page<HistoryResponse> getHistory(Pageable pageable, Long updatedById, Long ticketId, String updateType) {
+        log.info("[티켓 이력 조회] 티켓 ID: {}, 수정 유형: {}, 수정자: {}", ticketId, updateType, updatedById);
         return historyRepository.getHistory(pageable, updatedById, ticketId, updateType);
     }
 
     @Transactional
-    public void recordHistory(Ticket ticket, User user, TicketHistory.UpdateType updateType){
+    public void recordHistory(Ticket ticket, User user, TicketHistory.UpdateType updateType) {
+        log.info("[티켓 이력 기록] 티켓 ID: {}, 수정 유형: {}, 수정자: {}", ticket.getId(), updateType, user.getId());
        TicketHistory history = TicketHistory.createHistory(ticket,user,updateType);
         historyRepository.save(history);
     }
