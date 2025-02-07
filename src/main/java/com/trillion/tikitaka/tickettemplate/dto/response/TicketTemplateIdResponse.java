4f35aa0d5a7a2ddc4ea5864ee8c660211374137d
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-07 15:56:29 +0900

    [TKT-133] feat: 템플릿 생성시 아이디 값 DTO에 담아서 반환

---
+++ src/main/java/com/trillion/tikitaka/tickettemplate/dto/response/TicketTemplateIdResponse.java
@@ -0,0 +1,12 @@
+package com.trillion.tikitaka.tickettemplate.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketTemplateIdResponse {
+    private Long templateId;
+}
