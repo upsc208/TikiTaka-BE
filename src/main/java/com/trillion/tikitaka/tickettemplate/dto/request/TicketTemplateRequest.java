86c7fa5b00e3e2b02841735cd957019a1133500b
kangai1 <kmj104506@gachon.ac.kr>
2025-02-12 02:47:05 +0900

    [TKT-191] refactor: 티켓 템플릿 테스트 리펙토링

--- src/main/java/com/trillion/tikitaka/tickettemplate/dto/request/TicketTemplateRequest.java
+++ src/main/java/com/trillion/tikitaka/tickettemplate/dto/request/TicketTemplateRequest.java
@@ -1,28 +1,31 @@
 package com.trillion.tikitaka.tickettemplate.dto.request;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 @Getter
 @NoArgsConstructor
 public class TicketTemplateRequest {
-
     @NotBlank
     private String templateTitle;
-
     @NotBlank
     private String title;
-
     @NotBlank
     private String description;
-
     private Long typeId;
-
     private Long firstCategoryId;
-
     private Long secondCategoryId;
 
     private Long managerId;
+    public TicketTemplateRequest(Long typeId, Long firstCategoryId, Long secondCategoryId, Long managerId,
+                                 String templateTitle, String title, String description) {
+        this.typeId = typeId;
+        this.firstCategoryId = firstCategoryId;
+        this.secondCategoryId = secondCategoryId;
+        this.managerId = managerId;
+        this.templateTitle = templateTitle;
+        this.title = title;
+        this.description = description;
+    }
+
 }
