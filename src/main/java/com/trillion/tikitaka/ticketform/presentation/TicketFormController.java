f4b19f1ff4a1e2b6dfcc60e9bd82f2c768268dbb
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-12 15:39:56 +0900

    [TKT-195] fix: 티켓 폼 생성 시 필수 내용과 일반 내용 뒤바뀌는 오류 수정

--- src/main/java/com/trillion/tikitaka/ticketform/presentation/TicketFormController.java
+++ src/main/java/com/trillion/tikitaka/ticketform/presentation/TicketFormController.java
@@ -38,7 +38,7 @@ public class TicketFormController {
     public ApiResponse<Void> updateTicketForm(@PathVariable("firstCategoryId") Long firstCategoryId,
                                               @PathVariable("secondCategoryId") Long secondCategoryId,
                                               @RequestBody @Valid TicketFormRequest request) {
-        ticketFormService.updateTicketForm(firstCategoryId, secondCategoryId, request.getMustDescription(), request.getDescription());
+        ticketFormService.updateTicketForm(firstCategoryId, secondCategoryId, request.getDescription(), request.getMustDescription());
         return new ApiResponse<>(null);
     }
 
