b5d3126786e1ec469e155d72bfd862b7b3c3562a
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-13 12:16:18 +0900

    [TKT-208] fix: 비밀번호 변경 시 최대 비밀번호 32자리로 제한

--- src/main/java/com/trillion/tikitaka/user/dto/request/PasswordChangeRequest.java
+++ src/main/java/com/trillion/tikitaka/user/dto/request/PasswordChangeRequest.java
@@ -16,8 +16,8 @@ public class PasswordChangeRequest {
 
     @NotBlank
     @Pattern(
-            regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!]).{8,}$",
-            message = "비밀번호는 8자리 이상, 알파벳, 숫자, 특수문자를 포함해야 합니다."
+            regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!]).{8,32}$",
+            message = "비밀번호는 8자리 이상 32자리 이하이며 알파벳, 숫자, 특수문자를 포함해야 합니다."
     )
     private String newPassword;
 
