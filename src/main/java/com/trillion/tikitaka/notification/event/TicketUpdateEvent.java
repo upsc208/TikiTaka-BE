30f1107005bcebd71ea941d2ee2c43edea71264d
Hojun Im <ghwnsdla8094@gmail.com>
2025-02-15 23:53:43 +0900

    [TKT-227] fix: 티켓 객체를 티켓 스냅샷 DTO 객체로 변경

--- src/main/java/com/trillion/tikitaka/notification/event/TicketUpdateEvent.java
+++ src/main/java/com/trillion/tikitaka/notification/event/TicketUpdateEvent.java
@@ -3,6 +3,7 @@ package com.trillion.tikitaka.notification.event;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.trillion.tikitaka.notification.domain.NotificationType;
 import com.trillion.tikitaka.ticket.domain.Ticket;
+import com.trillion.tikitaka.ticket.dto.response.TicketSnapShot;
 import com.trillion.tikitaka.user.domain.Role;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 public class TicketUpdateEvent extends ApplicationEvent implements NotificationEvent {
     private final String email;
-    private final Ticket ticket;
+    private final TicketSnapShot ticketSnapshot;
     private final String modifier;
     private final String modification;
     private final Role modifierRole;
@@ -24,7 +25,14 @@ public class TicketUpdateEvent extends ApplicationEvent implements NotificationE
     public TicketUpdateEvent(Object source, String email, Ticket ticket, String modifier, String modification, Role modifierRole) {
         super(source);
         this.email = email;
-        this.ticket = ticket;
+        this.ticketSnapshot = new TicketSnapShot(
+                ticket.getId(),
+                ticket.getTitle(),
+                ticket.getFirstCategory() != null ? ticket.getFirstCategory().getName() : null,
+                ticket.getSecondCategory() != null ? ticket.getSecondCategory().getName() : null,
+                ticket.getTicketType() != null ? ticket.getTicketType().getName() : "",
+                ticket.getManager() != null ? ticket.getManager().getUsername() : null
+        );
         this.modifier = modifier;
         this.modification = modification;
         this.modifierRole = modifierRole;
